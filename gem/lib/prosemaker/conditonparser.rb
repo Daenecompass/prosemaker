b2ae3552e3fe41a8ee8b1652363f7d26d0ede0c7
Daniel Baird <daniel@danielbaird.com>
2013-10-24 20:54:28 +1000

    resolve condition kinda working

--- gem/lib/prosemaker/conditonparser.rb
+++ gem/lib/prosemaker/conditonparser.rb
@@ -1,37 +1,71 @@
 
 require 'parslet'
 
-class ProseCondition < Parslet::Parser
+# --------------------------------------------------------------
+class ProseConditionParser < Parslet::Parser
 
   # first define whitespace and other bits
-  rule(:space)     { match('\s').repeat(1) }
-  rule(:space?)    { space.maybe }
-  rule(:digits)    { match('[0-9]').repeat(1) }
+  rule(:space)      { match('\s').repeat(1) }
+  rule(:space?)     { space.maybe }
+  rule(:digits)     { match('[0-9]').repeat(1) }
 
   # atoms (including trailing spaces)
-  rule(:number)    { ( digits >> (str('.') >> digits).maybe ).as(:number) >> space? }
-  rule(:name)      { match('[_a-zA-Z]').repeat(1).as(:name) >> space? }
-  rule(:qstring)   { (str('"') >> match('[^"]').repeat(1).as('string') >> str('"')) >> space? }
-  rule(:dqstring)  { (str("'") >> match("[^']").repeat(1).as('string') >> str("'")) >> space? }
+  rule(:number)     { ( digits >> (str('.') >> digits).maybe ).as(:number) >> space? }
+  rule(:name)       { match('[_a-zA-Z]').repeat(1).as(:name) >> space? }
+  rule(:qstring)    { (str('"') >> match('[^"]').repeat(1).as('string') >> str('"')) >> space? }
+  rule(:dqstring)   { (str("'") >> match("[^']").repeat(1).as('string') >> str("'")) >> space? }
 
-  # integer or string
-  rule(:something) { number | name | qstring | dqstring }
+  rule(:cmp)        { ( match('[<>]') | str('==') ).as(:cmp) >> space? }
+
+  # integer or varname or string
+  rule(:something)  { number | name | qstring | dqstring }
+
+  # comparisons
+  rule(:comparison) { something.as(:left) >> cmp >> something.as(:right) }
+  rule(:condition)  { comparison }
 
   # root of a condition
-  root(:something)
+  root(:condition)
 end
+# --------------------------------------------------------------
+class ProseConditionResolver < Parslet::Transform
 
+  def initialize(data=nil)
+    super()
+    @data = data
+    puts @data
+  end
 
+#  rule(:name   => simple(:varname)) { varname }
+end
+# --------------------------------------------------------------
 def parse(str)
-  pc = ProseCondition.new
-  pc.parse(str)
-
+  pl = ProseConditionParser.new
+  pl.parse(str)
 rescue Parslet::ParseFailed => failure
   puts failure.cause.ascii_tree
 end
+# --------------------------------------------------------------
+def resolve(str, data)
 
-puts parse('asdf')
-puts parse('234')
-puts parse('2.34')
-puts parse('"as\'df"')
-puts parse("'a\"df'")
+  puts 'resolving conditon: ' + str
+  puts 'using data: ' + data.to_s
+
+  pl = ProseConditionParser.new
+  pp = Parslet::Transform.new do
+
+    # replace
+    rule(:number => simple(:numstr))  { Float(numstr) }
+    rule(:name => simple(:varname))   { |d| data[d[:varname].to_s] }
+
+    rule(:left => simple(:l), :cmp => '<', :right => simple(:r)) { l < r }
+  end
+  pp.apply(pl.parse(str))
+rescue Parslet::ParseFailed => failure
+  puts failure.cause.ascii_tree
+end
+# --------------------------------------------------------------
+puts resolve('a < 10', { 'a' => 8 })
+puts
+puts resolve('a < 10', { 'a' => 12 })
+
