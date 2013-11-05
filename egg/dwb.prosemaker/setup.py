cfaf68c2eee8a0afdbc52e58ae5d49f671a57a16
Daniel Baird <daniel@danielbaird.com>
2013-11-05 13:10:27 +1000

    Adds python egg structure, but no code lol

---
+++ egg/dwb.prosemaker/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n'
+)
+
+setup(
+    name='dwb.prosemaker',
+    version=version,
+    description="Document assembly with numeric conditions for scientific reporting.",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=["Programming Language :: Python"],
+    keywords='',
+    author='Daniel Baird',
+    author_email='daniel@danielbaird.com',
+    url='https://github.com/DanielBaird/prosemaker',
+    license='Apache',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['dwb'],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
